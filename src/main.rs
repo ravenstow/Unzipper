b36f80c6ed46f02b729f7851c6c95cfc02e67658
ravenstow <wvyu7231@gmail.com>
2024-03-15 13:35:20 +0800

    判斷路徑/檔案功能、過濾壓縮檔功能(待加入進入子路徑尋找壓縮檔案功能)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use dialoguer::{Input, Select};
 use std::path::PathBuf;
+use std::path::Path;
 use std::fs;
 
 fn main() {
     println!("Welcome to Unzipper!\n---");
+    // Get target path from prompts
     let file_path: PathBuf = prompt_to_get_path();
-    let file_names = get_file_names(file_path);
 
+    // Find out all file names under given dir path
+    let file_names = get_file_names(file_path);
+    let filtered_file_name = filter_archive_file(file_names);
+    // Check if has .rar/.zip/.7z
+    if filtered_file_name.is_empty() {
+        println!("There's no archive file under this directory or by given file name.\nTerminating...");
+        return
+    }
+
+    // Process unzipping work
     let extract = Select::new()
     .with_prompt("Select your extract method")
     .item("Method 1")
     .item("Method 2")
     .item("Method 3");
 
-
-
-    // let target_file_names = prompt_to_get_file_names()
+    // Try to look for password txt file for unarchive
 
     println!("Unzip files were done! Goodbye!\nUnzipper is ending...");
 }
@@ -39,6 +48,18 @@ fn prompt_to_get_path() -> PathBuf {
 fn get_file_names(target_path: PathBuf) -> Vec<String> {
     let mut array: Vec<String> = Vec::new();
 
+    // Check if path is dir/file
+    if target_path.is_file() {
+        println!("Receiving a file path...");
+        if let Some(file_name) = target_path.to_str() {
+            println!("Got file path: {}", file_name);
+            array.push(String::from(file_name));
+        }
+
+        return array;
+    }
+
+    println!("Receiving a directory, taking out file names under it...");
     if let Ok(entries) = fs::read_dir(target_path) {
         for entry in entries {
             if let Ok(entry) = entry {
@@ -51,4 +72,18 @@ fn get_file_names(target_path: PathBuf) -> Vec<String> {
     }
 
     return array;
+}
+
+fn filter_archive_file(path_names: Vec<String>) -> Vec<String> {
+    let mut valid_path_names: Vec<String> = Vec::new();
+    for path_name in path_names {
+        let path = Path::new(&path_name);
+        if let Some(ext) = path.extension() {
+            if ext == "rar" || ext == "7z" || ext == "zip" {
+                valid_path_names.push(path_name);
+            }
+        }
+    }
+
+    return valid_path_names;
 }
